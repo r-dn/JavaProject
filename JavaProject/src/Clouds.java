e7bcdf4789ec36753fffaadac087a9d0a242e6c4
Yamagsumi <wassimmghzr@gmail.com>
2020-04-11 13:53:22 +0200

    nieuwe klasse aangemaakt

--- JavaProject/src/Clouds.java
+++ JavaProject/src/Clouds.java
@@ -1,17 +1,15 @@
 import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Toolkit;
 
 public class Clouds {
-	
+
 	private int ScreenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
 	private int ScreenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
-	private Color cloudColor = new Color(255,255,255);
-	
-	public void draw(Graphics g){
-		Graphics2D g2D = (Graphics2D) g;
-		
+	private Color cloudColor = new Color(255, 255, 255);
+
+	public void draw(Graphics2D g2D) {
+
 		g2D.setColor(cloudColor); // Left cloud
 		g2D.fillOval((int) 50, (int) 50, (int) ScreenWidth / 9, (int) ScreenHeight / 9); // Left oval
 		g2D.fillOval((int) 100, (int) 10, (int) ScreenWidth / 9, (int) ScreenHeight / 9); // Upper oval
@@ -29,7 +27,7 @@ public class Clouds {
 		g2D.fillOval((int) 1300, (int) 10, (int) ScreenWidth / 10, ScreenHeight / 10);
 		g2D.fillOval((int) 1300, (int) 75, ScreenWidth / 10, ScreenHeight / 10);
 		g2D.fillOval((int) 1350, (int) 50, (int) ScreenWidth / 10, ScreenHeight / 10);
-		
+
 		g2D.setColor(Color.YELLOW); // Sun
 		g2D.fillOval((int) 800, (int) 20, (int) ScreenWidth / 8, (int) ScreenWidth / 8);
 
